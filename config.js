5849d3500a992ae597facc136b7f54260756e830
Dodangiu <88225211+Dodangiu@users.noreply.github.com>
2021-07-31 22:18:58 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,15 @@
 const CONFIG = {
-    introTitle: 'Tên người yêu bạn à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    introTitle: 'Heluuuuu cậu dth!',
+    introDesc: `Tớ có điều này muốn hỏi cậu nhớ phải trả lời thật lòng nhoaaaa!`,
+    btnIntro: 'Okieee',
+    title: 'Cậu yêu tớ có phải hong nàoooo ._.',
+    desc: 'Nếu cậu hong trả lời mà thoát ra tức là cậu iu tớ đấy nháaa :3',
+    btnYes: 'Iu ơi là iuuuu <33',
+    btnNo: 'Cậu mơ àaa :v',
+    question:'Nói cho tớ biết lí do cậu iu tớ đii ^^',
+    btnReply: 'Gửi cho tớ <3',
+    reply: 'Cậu xink thế kia hong iu thì phí :>>>>>>>>>>>>',
+    mess: 'Tớ biết mà ^^ Love u 3 thousand  nàa',
+    messDesc: 'Tối nay rảnh thì videocall với tớ nhaaa :3 Còn chần chừ gì nữa mà ko ib tớ đi nàoooo',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
-}
+    messLink: 'http://m.me/100070504141368'
